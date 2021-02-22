a4cd1fd9144f3ac6bcb2a3ec19fc2f2e6ccba5ae
DidDrog11 <dsimons1987@gmail.com>
2021-02-22 09:32:57 +0000

    v11 setup

--- scripts/ovid_search.R
+++ scripts/ovid_search.R
@@ -1,6 +1,13 @@
 library("tidyverse")
 library("here")
+library(magrittr)
+library(bib2df)
+library(revtools)
 
 ovid_dec <- read_csv(here("data_spreadsheet", "2020-12-17_ovid.csv")) %>%
   select(DO) %>%
   drop_na()
+
+read_bibliography(here("data_spreadsheet", "ovid_2021-02-16.ris")) %>%
+  filter(!doi %in% ovid_dec$DO) %>%
+  write_bibliography(., file = here("data_spreadsheet", "deduplicated_ovid.bib"))
